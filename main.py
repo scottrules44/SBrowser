3743f828b7042d836f4d06e662e7e06ad2534127
Scott Harrison <scottrules44@gmail.com>
2023-03-10 20:50:12 -0600

    Fixes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWebEngineWidgets import *
+from PySide6.QtWidgets import *
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWebEngineWidgets import *
 
 startPage = "https://google.com"
 
